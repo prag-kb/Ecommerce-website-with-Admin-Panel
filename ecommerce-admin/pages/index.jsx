a890f692c4b3ae5125828b6dee950d9abf41c155
pragkb <kbprabu6@gmail.com>
2024-06-16 20:42:35 +0530

    initial commit

---
+++ ecommerce-admin/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (   
+    <div className="bg-blue-900 w-screen flex h-screen items-center">
+      <div className="text-center w-full">
+      <button className="bg-white p-2 rounded-md px-4">Login with Google</button>
+      </div>
+    </div>
+  )
+}
